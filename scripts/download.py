76e31b417f4c3b9865c68785f9e927e487142bfb
hytian2@gmail.com <sky24h>
2023-09-25 18:08:03 +0900

    fix download

--- scripts/download.py
+++ scripts/download.py
@@ -5,11 +5,12 @@ with open(os.path.join(os.path.dirname(__file__), "download_list.txt"), "r") as 
     download_list = r.read().splitlines()
 
 for download in download_list:
+    print(download)
     repo_id = "stabilityai/" + download.split("/")[1]
-    subfolder = download.split("/")[2]
+    subfolder = download.split("/")[2] if len(download.split("/")) > 3 else None
     filename = download.split("/")[-1]
 
     local_dir = download.split("/")[0] + "/" + download.split("/")[1]
     local_dir = os.path.join(os.path.dirname(__file__), "..", local_dir)
-
+    print("Downloading from {}/{}/{} to {}".format(repo_id, subfolder, filename, local_dir))
     hf_hub_download(repo_id=repo_id, filename=filename, subfolder=subfolder, local_dir=local_dir, local_dir_use_symlinks=False)
